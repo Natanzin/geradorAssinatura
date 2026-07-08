88cf12d8c10ecb29ae071f4f9b89e1fc2acf76c3
Natanzin <natan.silva.judo@gmail.com>
2026-07-08 16:04:38 -0300

    Commit de implantação

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function atualizar() {
     document.getElementById("previewNome").innerHTML = nome.value;
     document.getElementById("previewCargo").innerHTML = cargo.value;
     document.getElementById("previewSetor").innerHTML = setor.value;
-    document.getElementById("previewRamal").innerHTML = "Ramal: " + ramal.value;
+    document.getElementById("previewRamal").innerHTML = "(61) 3251-" + ramal.value;
 }
 
 nome.addEventListener("input", atualizar);
@@ -15,34 +15,24 @@ cargo.addEventListener("input", atualizar);
 setor.addEventListener("input", atualizar);
 ramal.addEventListener("input", atualizar);
 
-function gerarHTML() {
-    const html = `
-<table cellpadding="0" cellspacing="0" border="0"
-style="
-width:520px;
-height:170px;
-background-image:url('assinatura.png');
-background-size:cover;
-font-family:Arial;
-position:relative;
-">
-<tr>
-<td style="position:relative;width:520px;height:170px;">
-<div style="position:absolute;left:210px;top:38px;font-size:22px;font-weight:bold;">
-${nome.value}
-</div>
-<div style="position:absolute;left:210px;top:70px;font-size:14px;">
-${cargo.value}
-</div>
-<div style="position:absolute;left:210px;top:92px;font-size:13px;">
-${setor.value}
-</div>
-<div style="position:absolute;left:210px;top:114px;font-size:13px;">
-Ramal: ${ramal.value}
-</div>
-</td>
-</tr>
-</table>
-`;
-    document.getElementById("codigo").value = html;
+async function exportarDivComoJPG(idDiv, nomeArquivo = "assinatura.jpg") {
+    const elemento = document.getElementById(idDiv);
+
+    if (!elemento) {
+        console.error("Div não encontrada.");
+        return;
+    }
+
+    const canvas = await html2canvas(elemento, {
+        scale: 3,              // melhora a qualidade
+        useCORS: true,
+        backgroundColor: "#ffffff"
+    });
+
+    const imagem = canvas.toDataURL("image/jpeg", 1.0);
+
+    const link = document.createElement("a");
+    link.href = imagem;
+    link.download = nomeArquivo;
+    link.click();
 }
